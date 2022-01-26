0e67c99a7ce3b0d1f70d52c58419db2c53188215
Carsten <carsteno@uia.no>
2022-01-26 11:36:00 +0100

    SomeJavaStuff

---
+++ main.java
@@ -0,0 +1,50 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hey guys");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        long num = 368069660;
+        long divisor = 2, largestPrimeFactor;
+        while (num != 0) {
+            if (num % divisor != 0) {
+                divisor++;
+            } else {
+                largestPrimeFactor = num;
+                num /= divisor;
+                if (num == 1) {
+                    System.out.println("The largest prime factor: " + largestPrimeFactor);
+                    break;
+                }
+            }
+        }*/
+
+
+
+       
+    
+
+
+
