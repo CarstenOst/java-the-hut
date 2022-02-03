be9500a95788d9f321e966020da666a5eec5cc6e
Carsten Østergaard <carsteno@uia.no>
2022-02-03 20:13:38 +0100

    Hola title

--- MyFrame.java
+++ MyFrame.java
@@ -3,11 +3,11 @@ import java.awt.Color;
 
 public class MyFrame extends JFrame {
 
-    MyFrame(){
-
-        this.setTitle("Hola mi amor");
+    MyFrame()
+    {
+        this.setTitle("Hola!");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                // Avslutte når man avslutte
-        this.setResizable(true);                                          // Kan ikke endre størrelsen av framen
+        this.setResizable(true);                                           // Kan ikke endre størrelsen av framen
         this.setSize(420,420);                                // Bredde og høyde
         this.setVisible(true);                                           // Gjør den synlig
         this.getContentPane().setBackground(new Color(0xFFD700));   // Farge
